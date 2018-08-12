a989b7a63378b4bca69ea3ab8c1aa7139928f1e2
rupesh2017 <rupesh.sak94@gmail.com>
2018-08-12 17:06:31 +0530

    flask book review app

--- application.py
+++ application.py
@@ -18,13 +18,9 @@ db = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
 
-
 app.config["SESSION_PERMANENT"]=False
 app.config["SESSION_TYPE"]="filesystem"
 Session(app)
-
-
-
 
 
 @app.route("/")
